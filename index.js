c1f7330de5843ba9a54ae19d96e93c22bd75394f
aaalzand <48525345+aaalzand@users.noreply.github.com>
2020-07-12 18:00:44 -0400

    Update index.js
    Update the logic to avoid a situation when all instances are compliance.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var AWS = require('aws-sdk');
-AWS.config.update({region: process.env.AWS_REGION});
+AWS.config.update({ region: process.env.AWS_REGION });
 
 var ssm = new AWS.SSM();
 var s3 = new AWS.S3();
@@ -9,124 +9,124 @@ var sts = new AWS.STS();
 var listItemsParams = {
     Filters: [
         {
-            "Key":"AWS:ComplianceItem.Status",
-            "Values":["NON_COMPLIANT"],
-            "Type":"Equal"
+            "Key": "AWS:ComplianceItem.Status",
+            "Values": ["NON_COMPLIANT"],
+            "Type": "Equal"
         },
         {
-            "Key":"AWS:ComplianceItem.ComplianceType",
-            "Values":["Patch"],
-            "Type":"Equal"
+            "Key": "AWS:ComplianceItem.ComplianceType",
+            "Values": ["Patch"],
+            "Type": "Equal"
         },
         {
-             "Key":"AWS:ComplianceItem.Severity",
-             "Values":["CRITICAL"],
-             "Type":"Equal"
-         }
+            "Key": "AWS:ComplianceItem.Severity",
+            "Values": ["CRITICAL"],
+            "Type": "Equal"
+        }
     ],
-    ResourceTypes:[
+    ResourceTypes: [
         "ManagedInstance"
     ],
-	ResourceIds:[
-		"InstanceID"
-	]
+    ResourceIds: [
+        "InstanceID"
+    ]
 }
 var complianceResourceSummaryParams = {
     Filters: [
-        { 
-            Key: "AWS:ComplianceSummary.Status", 
-            Values: ["NON_COMPLIANT" ],
-            Type: "Equal" 
-        }, 
-        { 
-            Key: "AWS:ComplianceSummary.ComplianceType", 
+        {
+            Key: "AWS:ComplianceSummary.Status",
+            Values: ["NON_COMPLIANT"],
+            Type: "Equal"
+        },
+        {
+            Key: "AWS:ComplianceSummary.ComplianceType",
             Values: ["Patch"],
-            Type: "Equal" 
+            Type: "Equal"
         }
     ]
 }
 function systemManagerSyncSecurityHub(resolve, reject) {
-        ssm.listResourceComplianceSummaries(complianceResourceSummaryParams, function(err, complianceSummary) {
-        if(err) {
+    ssm.listResourceComplianceSummaries(complianceResourceSummaryParams, function (err, complianceSummary) {
+        if (err) {
             console.log(err);
             reject(err);
             return;
         }
-        var resources = [];
-        complianceSummary.ResourceComplianceSummaryItems.forEach(function(singleComplianceItem) {
-            if(singleComplianceItem.ResourceType && 'ManagedInstance' === (singleComplianceItem.ResourceType)) {
-                resources.push(singleComplianceItem.ResourceId);
-            }
-        });
-        
-        listItemsParams.ResourceIds = resources;
 
-        ssm.listComplianceItems(listItemsParams, function(err, complianceItems) {
-            if (err) {
-                console.log(err, err.stack); 
-                
-            reject(err);
-            }
+        if (complianceSummary.ResourceComplianceSummaryItems[0] != undefined) {
+            var resources = [];
+            complianceSummary.ResourceComplianceSummaryItems.forEach(function (singleComplianceItem) {
+                if (singleComplianceItem.ResourceType && 'ManagedInstance' === (singleComplianceItem.ResourceType)) {
+                    resources.push(singleComplianceItem.ResourceId);
+                }
+            });
 
-            sts.getCallerIdentity({},function(err,stsData) {
-                if(err) {
-                    console.log(err);
+            listItemsParams.ResourceIds = resources;
+
+            ssm.listComplianceItems(listItemsParams, function (err, complianceItems) {
+                if (err) {
+                    console.log(err, err.stack);
                     reject(err);
-                    return;
-                }    
-                var missingPatches = getMissingPatches(complianceItems);
+                }
 
-                var findings = [];
-                Object.values(missingPatches).forEach(function(singleComplianceItemFinding) {
-                    findings.push(getSingleComplianceItemFinding(singleComplianceItemFinding, stsData));
-                });
-                console.log(JSON.stringify(findings));
-                
-                securityhub.batchImportFindings({
-                    'Findings': findings
-                }, function(err, secHubData) {
+                sts.getCallerIdentity({}, function (err, stsData) {
                     if (err) {
+                        console.log(err);
                         reject(err);
                         return;
-                    } 
-                    else{
-                      resolve(secHubData);
-                    }           
+                    }
+                    var missingPatches = getMissingPatches(complianceItems);
+
+                    var findings = [];
+                    Object.values(missingPatches).forEach(function (singleComplianceItemFinding) {
+                        findings.push(getSingleComplianceItemFinding(singleComplianceItemFinding, stsData));
+                    });
+                    console.log(JSON.stringify(findings));
+
+                    securityhub.batchImportFindings({
+                        'Findings': findings
+                    }, function (err, secHubData) {
+                        if (err) {
+                            reject(err);
+                            return;
+                        }
+                        else {
+                            resolve(secHubData);
+                        }
+                    });
+
                 });
+
             });
-            
-        });
+        } else {
+            console.log("There is no instance with missing patches and compliance severity set as critical")
+            return;
+        }
     });
 }
-var getSingleComplianceItemFinding = function(missingPatch, stsData) {
-    
+var getSingleComplianceItemFinding = function (missingPatch, stsData) {
     return {
-                    AwsAccountId: stsData.Account,
-                    CreatedAt: new Date().toISOString(),
-                    Description: 'A critical patch is missing in some of the instances. Check further details in Systems Manager Compliance \n Patch Title: ' + missingPatch.Title + '\n Patch Id: ' + missingPatch.Id,
-                    GeneratorId: 'SSM',
-                    Id:  missingPatch.Title,
-                    ProductArn: 'arn:aws:securityhub:'+process.env.AWS_REGION+':'+stsData.Account+':product/'+stsData.Account+'/default', 
-                    Resources: missingPatch.resources,
-                    SchemaVersion: "2018-10-08",
-                    Severity: { /* required */
-                        Normalized: '70' // TODO
-                    },
-                    Title: 'Critical Patch with title ' + missingPatch.Title + ' missing',
-                    UpdatedAt: new Date().toISOString(), /* required */
-                    Compliance: {
-                        Status: 'FAILED'
-                    },
-                    Types: [
-                        'PATCH'
-                    ],
+        AwsAccountId: stsData.Account,
+        CreatedAt: new Date().toISOString(),
+        Description: 'A critical patch is missing in some of the instances. Check further details in Systems Manager Compliance \n Patch Title: ' + missingPatch.Title + '\n Patch Id: ' + missingPatch.Id,
+        GeneratorId: 'SSM',
+        Id: missingPatch.Title,
+        ProductArn: 'arn:aws:securityhub:' + process.env.AWS_REGION + ':' + stsData.Account + ':product/' + stsData.Account + '/default',
+        Resources: missingPatch.resources,
+        SchemaVersion: "2018-10-08",
+        Severity: { /* required */
+            Normalized: '70' // TODO
+        },
+        Title: 'Critical Patch with title ' + missingPatch.Title + ' missing',
+        UpdatedAt: new Date().toISOString(), /* required */
+        Compliance: {
+            Status: 'FAILED'
+        },
+        Types: [
+            'PATCH'
+        ],
     }
-    
 }
-
-
-
-
 
 function getMissingPatches(complianceItems, Title, map) {
     var missingPatches = {};
@@ -151,7 +151,7 @@ function getMissingPatches(complianceItems, Title, map) {
 }
 
 exports.handler = async (event) => {
-	var InstanceID = event.instnaceid
-	console.log (InstanceID)
-    return new Promise(systemManagerSyncSecurityHub);    
+    var InstanceID = event.instnaceid
+    console.log(InstanceID)
+    return new Promise(systemManagerSyncSecurityHub);
 }
